7203bbfb64fbc207fa13a5778322496342525cee
Brandon Travis <travisbrandont@gmail.com>
2021-05-05 03:22:33 +1000

    Skinny 1 it is

--- pages/index.js
+++ pages/index.js
@@ -107,7 +107,7 @@ export default function Home() {
                         <Hidden smDown>
                             <Grid item xs={12} sm={12} md={8} lg={8} xl={8} >
                                 <Box display="flex" alignItems="flex-end" justifyContent="center" style={{ height: '95vh' }}>
-                                    <img src="/remote.png" style={{ width: '100rem', height: '824px' }} />
+                                    <Image src="/remote.png" width={909} height={899} />
                                 </Box>
                             </Grid>
                         </Hidden>
