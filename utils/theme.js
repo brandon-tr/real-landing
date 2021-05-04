b6cde83d045cbef6edaa6e96b881c38bab366e50
Brandon Travis <travisbrandont@gmail.com>
2021-05-05 02:36:15 +1000

    123

---
+++ utils/theme.js
@@ -0,0 +1,28 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+import typography from "./typography";
+
+// Create a theme instance.
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#6D58A5',
+            dark: '#342F51',
+            light: '#8D78B7'
+        },
+        secondary: {
+            main: '#F0EFEE',
+            second: '#8ED8EF',
+        },
+        error: {
+            main: red.A400,
+        },
+        background: {
+            default: '#6D58A5',
+            secondary: '#F0EFEE'
+        },
+    },
+    typography
+});
+
+export default theme;
---
+++ utils/typography.js
@@ -0,0 +1,48 @@
+export default {
+    body1: {
+        fontFamily: '"work sans", "Helvetica", "Arial", sans-serif',
+        fontWeight: 500,
+        fontSize: '1rem',
+        letterSpacing: '-0.05px'
+    },
+    h1: {
+        fontFamily: '"work sans", "Helvetica", "Arial", sans-serif',
+        fontWeight: 500,
+        fontSize: 35,
+        letterSpacing: '-0.24px'
+    },
+    h2: {
+        fontFamily: '"work sans", "Helvetica", "Arial", sans-serif',
+        fontWeight: 500,
+        fontSize: 29,
+        letterSpacing: '-0.24px'
+    },
+    h3: {
+        fontFamily: '"work sans", "Helvetica", "Arial", sans-serif',
+        fontWeight: 500,
+        fontSize: 24,
+        letterSpacing: '-0.06px'
+    },
+    h4: {
+        fontFamily: '"work sans", "Helvetica", "Arial", sans-serif',
+        fontWeight: 500,
+        fontSize: 20,
+        letterSpacing: '-0.06px'
+    },
+    h5: {
+        fontFamily: '"work sans", "Helvetica", "Arial", sans-serif',
+        fontWeight: 500,
+        fontSize: 16,
+        letterSpacing: '-0.05px'
+    },
+    h6: {
+        fontFamily: '"work sans", "Helvetica", "Arial", sans-serif',
+        fontWeight: 500,
+        fontSize: 14,
+        letterSpacing: '-0.05px'
+    },
+    overline: {
+        fontFamily: '"work sans", "Helvetica", "Arial", sans-serif',
+        fontWeight: 500
+    }
+};
